0c3f81d18c0aaa3cefbdb9745efd7c8b3eab7e87
Ali Raza <muntazim1001@gmail.com>
2026-01-21 07:15:43 +0500

    remove /admin from path prefix

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('/')
             ->viteTheme('resources/css/filament/admin/theme.css')
             ->login()
             ->colors([
